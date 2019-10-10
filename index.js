54b6a5fedcce96a62b14b15c5b6ee2093b84db95
unknown <thinh.ngtruong@gmail.com>
2019-10-10 14:07:24 +0700

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var _findIndex = require('lodash/findIndex') // npm install lodash --save
 var server = require('http').Server(app);
-var port = (4000);
+var port = (process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000);
 var io = require('socket.io')(server);
 server.listen(port, () => console.log('Server running in port ' + port));
 
