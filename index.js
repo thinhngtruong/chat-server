a642de26bfc64143c77c2ee20fd2c77429be9d71
unknown <thinh.ngtruong@gmail.com>
2019-10-21 00:01:00 +0700

    update

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ var io = require('socket.io')(server);
 server.listen(port, () => console.log('Server running in port ' + port));
 
 var userOnline = []; //danh sách user dang online
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log(socket.id + ': connected');
     //lắng nghe khi người dùng thoát
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log(socket.id + ': disconnected')
         $index = _findIndex(userOnline, ['id', socket.id]);
         userOnline.splice($index, 1);
         io.sockets.emit('updateUserList', userOnline);
-    })
+    }) 
     //lắng nghe khi có người gửi tin nhắn
     socket.on('newMessage', data => {
         //gửi lại tin nhắn cho tất cả các user dang online
         io.sockets.emit('newMessage', {
             data: data.data,
             user: data.user
-        });
+        }); 
     })
     //lắng nghe khi có người login
     socket.on('login', data => {
@@ -39,16 +39,16 @@ io.on('connection', function(socket) {
             io.sockets.emit('updateUserList', userOnline);// gửi danh sách user dang online
         }
     })
+
     socket.on("typing", data => {
         var s = data.user + " đang nhập văn bản"
-        socket.broadcast.emit("dangnhap",s)
+        socket.broadcast.emit("dangnhap", s)
     })
 
-    socket.on("stop-typing",function(){
-        var s=""
-        socket.broadcast.emit("dungnhap",s)
+    socket.on("stop-typing", function () {
+        var s = ""
+        socket.broadcast.emit("dungnhap", s)
     })
-
 });
 
 app.get('/', (req, res) => {
